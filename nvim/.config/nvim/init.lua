1c2109cafa39fcf3250de0954caa6c1d5f479f44
astr0n0mer <42691857+astr0n0mer@users.noreply.github.com>
2024-10-21 22:02:16 +0530

    fix: tries to fix tsserver issue with neovim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -616,16 +616,20 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
+
+        -- python
         -- pyright = {},
         basedpyright = {},
+
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {},
+        -- biome = {},
+        -- But for many setups, the LSP (`ts_ls`) will work just fine
+        ts_ls = {},
         --
 
         lua_ls = {
@@ -666,9 +670,13 @@ require('lazy').setup({
             local server = servers[server_name] or {}
             -- This handles overriding only values explicitly passed
             -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for tsserver)
+            -- certain features of an LSP (for example, turning off formatting for ts_ls)
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
             require('lspconfig')[server_name].setup(server)
+          end,
+          -- INFO: https://github.com/williamboman/mason.nvim/issues/1784#issuecomment-2335008233
+          ts_ls = function()
+            require('lspconfig')['tsserver'].setup {}
           end,
         },
       }
