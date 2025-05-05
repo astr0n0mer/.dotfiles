f396f2c242f350397e77d5202ddffff179057eb4
astr0n0mer <42691857+astr0n0mer@users.noreply.github.com>
2025-05-05 22:46:04 +0530

    cleanup: trims lazygit config

--- nvim/.config/nvim/lua/custom/plugins/zk-nvim.lua
+++ nvim/.config/nvim/lua/custom/plugins/zk-nvim.lua
@@ -1,8 +1,8 @@
 return {
-  'zk-org/zk-nvim',
-  config = function()
-    require('zk').setup {
-      -- See Setup section below
-    }
-  end,
+  -- 'zk-org/zk-nvim',
+  -- config = function()
+  --   require('zk').setup {
+  --     -- See Setup section below
+  --   }
+  -- end,
 }
