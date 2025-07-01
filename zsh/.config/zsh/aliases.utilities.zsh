482ee3ce1d70f5f80a2cbd1e795caaafa0f846d8
astr0n0mer <42691857+astr0n0mer@users.noreply.github.com>
2025-07-01 23:27:22 +0530

    refactor: replaces tampermonkey with violentmonkey

--- zsh/.config/zsh/aliases.utilities.zsh
+++ zsh/.config/zsh/aliases.utilities.zsh
@@ -34,14 +34,15 @@ multilog() {
 
 # INFO: screenshot
 SCREENSHOT_LOCATION="$HOME/Downloads/screenshots"
-ss(){
-    cd "$SCREENSHOT_LOCATION" || return 1
-    ls -t1 \
-        | head -n ${1:-20} \
-        | xargs -I {} realpath "{}"
-    popd > /dev/null
-}
-alias ssopen="ss | fzf | xargs -I {} open {}"
+# ss(){
+#     cd "$SCREENSHOT_LOCATION" || return 1
+#     ls -t1 \
+#         | head -n ${1:-20} \
+#         | xargs -I {} realpath "{}"
+#     popd > /dev/null
+# }
+# alias ssopen="ss | fzf | xargs -I {} open {}"
+alias ssopen="open ${SCREENSHOT_LOCATION}/Screenshot*"
 sscleanup() {
     find "$SCREENSHOT_LOCATION" -type f -mtime ${1:-+30} -exec trash -v "{}" \;
 }
