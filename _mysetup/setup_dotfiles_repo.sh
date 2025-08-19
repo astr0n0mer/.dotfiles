c7b48ac253581f4ebce8801dd72c42c29b3fb9fe
astr0n0mer <42691857+astr0n0mer@users.noreply.github.com>
2025-08-19 18:54:54 +0530

    fix: fixes error in setup script

--- _mysetup/setup_dotfiles_repo.sh
+++ _mysetup/setup_dotfiles_repo.sh
@@ -12,16 +12,17 @@ ssh-keyscan -H github.com gitlab.com >> ~/.ssh/known_hosts
 cd ~
 git clone --recurse-submodules https://github.com/astr0n0mer/.dotfiles.git
 cd .dotfiles
-git fetch --all
 git remote remove origin
 git remote add origin git@github.com:astr0n0mer/.dotfiles.git
-git branch --set-upstream-to=origin/main
+# git fetch --all INFO: cannot fetch, because ssh keys are not set up yet
+# git branch --set-upstream-to=origin/main
 
 # set up ssh keys
 cd dotfiles_sensitive/.ssh
 git remote remove origin
 git remote add origin git@gitlab.com:astr0n0mer/dotfiles_sensitive.git
-git branch --set-upstream-to=origin/main
+# git fetch --all # INFO: cannot fetch, because the repo is private
+# git branch --set-upstream-to=origin/main
 make decrypt
 cd ..
 make setup_with_dotfiles
