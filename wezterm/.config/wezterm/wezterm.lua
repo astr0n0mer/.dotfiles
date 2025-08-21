d1df36f63341806f863e1272cddf3721dce33189
astr0n0mer <42691857+astr0n0mer@users.noreply.github.com>
2025-08-21 11:24:37 +0530

    config: removes unused homebrew packages

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,18 +3,18 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.disable_default_key_bindings = true
+-- config.disable_default_key_bindings = true
 config.enable_tab_bar = false
 config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 12.5
 -- config.line_height = 0.9
-config.window_background_opacity = 0.75
+-- config.window_background_opacity = 0.75
 config.window_decorations = "RESIZE"
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 
-config.keys = {
-	{ mods = "CTRL", key = "L", action = wezterm.action.ShowDebugOverlay },
-	{ mods = "SUPER", key = "v", action = wezterm.action.PasteFrom("Clipboard") },
-}
+-- config.keys = {
+-- 	{ mods = "CTRL", key = "L", action = wezterm.action.ShowDebugOverlay },
+-- 	{ mods = "SUPER", key = "v", action = wezterm.action.PasteFrom("Clipboard") },
+-- }
 
 return config
