2b83200f40f9e08d47494124087fa842c0328a6b
astr0n0mer <20461113-astr0n0mer@users.noreply.gitlab.com>
2025-08-28 19:47:24 +0530

    fix: moves echasnovski's repos to nvim-mini
    - [Link to the migration
    commit](https://github.com/nvim-mini/mini.nvim/commit/07ab5a4dcd6a82ec31426a40f8f7e1fdce442934)

--- nvim/.config/nvim/lua/config/plugins/mini.nvim.lua
+++ nvim/.config/nvim/lua/config/plugins/mini.nvim.lua
@@ -1,6 +1,6 @@
 return {
   {
-    "echasnovski/mini.nvim",
+    "nvim-mini/mini.nvim",
     config = function()
       local statusline = require("mini.statusline")
       statusline.setup({ use_icons = true })
